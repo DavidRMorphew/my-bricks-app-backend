30d85ed3de114a97add973fc00a0431acf84daa4
DavidRMorphew <morphewd@umich.edu>
2021-06-23 16:17:17 -0400

    Api.find_or_create_theme_by_id added

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,6 +1,10 @@
 class Api < ApplicationRecord
     @@base_url = "https://rebrickable.com/api/v3/lego"
 
+    def self.find_or_create_theme_by_id(theme_id)
+        theme = (Theme.find_by(theme_number: theme_id) || self.fetch_theme_by_id(theme_id))
+    end
+    
     def self.fetch_theme_by_id(theme_id)
         url = "#{@@base_url}/themes/#{theme_id}/?key=#{ENV["LEGO_API_KEY"]}"
         uri = URI(url)
