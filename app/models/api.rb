3dd26b0658ea3672ff58491e539bd94db1151f8c
DavidRMorphew <morphewd@umich.edu>
2021-06-23 16:14:49 -0400

    Api.self.fetch_theme_by_id added and tested && app/models/concerns/api.rb removed

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,5 +1,13 @@
 class Api < ApplicationRecord
     @@base_url = "https://rebrickable.com/api/v3/lego"
 
-    
+    def self.fetch_theme_by_id(theme_id)
+        url = "#{@@base_url}/themes/#{theme_id}/?key=#{ENV["LEGO_API_KEY"]}"
+        uri = URI(url)
+
+        resp = Net::HTTP.get(uri)
+
+        theme = JSON.parse(resp)
+        Theme.create(theme_number: theme["id"], name: theme["name"])
+    end
 end
--- app/models/concerns/api.rb
+++
@@ -1,5 +0,0 @@
-class Api < ApplicationRecord
-    @@base_url = "https://rebrickable.com/api/v3/lego"
-
-    
-end
