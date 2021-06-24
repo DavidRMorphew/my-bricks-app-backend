701804a06618b0156d41c293a480baba79b641df
DavidRMorphew <morphewd@umich.edu>
2021-06-24 17:33:18 -0400

    id added to LegoSetsSerializer

--- app/serializers/lego_set_serializer.rb
+++ app/serializers/lego_set_serializer.rb
@@ -1,4 +1,4 @@
 class LegoSetSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :image_url, :instructions_url, :year, :owned, :potential_build, :total_bricks, :set_number
+  attributes :name, :image_url, :instructions_url, :year, :owned, :potential_build, :total_bricks, :set_number, :id
 end
