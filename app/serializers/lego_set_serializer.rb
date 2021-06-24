0c1bedcd46bb594d6618ab306fb0bc1bb6577924
DavidRMorphew <morphewd@umich.edu>
2021-06-24 17:36:41 -0400

    set_key_transform added to convert keys to camel case for json serializer

--- app/serializers/lego_set_serializer.rb
+++ app/serializers/lego_set_serializer.rb
@@ -1,4 +1,5 @@
 class LegoSetSerializer
   include FastJsonapi::ObjectSerializer
+  set_key_transform :camel_lower
   attributes :name, :image_url, :instructions_url, :year, :owned, :potential_build, :total_bricks, :set_number, :id
 end
