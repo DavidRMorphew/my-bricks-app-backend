a5de31daac39eb68b1f41e030009cfaaaa8db5bf
DavidRMorphew <morphewd@umich.edu>
2021-06-26 13:37:46 -0400

    LegoSet potential_builds_regardless_of_color class method returns all potential builds where part number of sets does not exceed owned parts of that type regardless of the part color

--- app/models/lego_set.rb
+++ app/models/lego_set.rb
@@ -89,4 +89,15 @@ class LegoSet < ApplicationRecord
             end
         end
     end
+
+    def self.potential_builds_regardless_of_color
+        owned_parts_quantity_ary = self.array_of_parts_and_quantity_objects_regardless_of_color_owned
+        filtered_sets_by_parts = self.potential_builds_regardless_of_color_filtered_by_part_types(owned_parts_quantity_ary)
+
+        filtered_sets_by_parts.select do |set|
+            set.array_parts_quantity_regardless_of_color.all? do |part_obj|
+                part_obj[:part_quantity] <= owned_parts_quantity_ary.detect{|owned_part| owned_part[:part_number] == part_obj[:part_number]}[:part_quantity]
+            end
+        end
+    end
 end
