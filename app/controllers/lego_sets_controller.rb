3e595220d8fe68c3e3be9de13ef67821762c4118
DavidRMorphew <morphewd@umich.edu>
2021-06-26 16:31:33 -0400

    update action for owned built

--- app/controllers/lego_sets_controller.rb
+++ app/controllers/lego_sets_controller.rb
@@ -9,4 +9,11 @@ class LegoSetsController < ApplicationController
         lego_sets = LegoSet.where(owned: true)
         render json: LegoSetSerializer.new(lego_sets)
     end
+
+    def update
+        lego_set = LegoSet.find_by(id: params[:id])
+        if lego_set.update(owned: true)
+            render json: LegoSetSerializer.new(lego_set)
+        end
+    end
 end
