39fb587e652cf53ab3ab41dc7f5c1e16a43a6850
DavidRMorphew <morphewd@umich.edu>
2021-06-26 14:43:45 -0400

    custom route lego_sets/owned created with controller action lego_sets#owned_sets that selects only sets that are owned:true

--- app/controllers/lego_sets_controller.rb
+++ app/controllers/lego_sets_controller.rb
@@ -4,4 +4,9 @@ class LegoSetsController < ApplicationController
         lego_sets = LegoSet.all
         render json: LegoSetSerializer.new(lego_sets)
     end
+
+    def owned_sets
+        lego_sets = LegoSet.where(owned: true)
+        render json: LegoSetSerializer.new(lego_sets)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lego_sets
+  resources :lego_sets, only: [:index, :update]
 
+  get "/lego_sets/owned", to: "lego_sets#owned_sets", as: "owned_sets"
 end
