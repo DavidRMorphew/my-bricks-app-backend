64ca0bc3065c2e3f8f080d7725a512b4bba840ad
DavidRMorphew <morphewd@umich.edu>
2021-06-25 23:37:59 -0400

    theme_name reader added to LegoSet model and added to LegoSetSerializer

--- app/models/lego_set.rb
+++ app/models/lego_set.rb
@@ -2,4 +2,8 @@ class LegoSet < ApplicationRecord
     belongs_to :theme
     has_many :set_part_specs
     has_many :parts, through: :set_part_specs
+
+    def theme_name
+        self.theme.name
+    end
 end
