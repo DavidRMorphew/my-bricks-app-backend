ce594e2dd0e375852aa9a34293c05b0218b0e025
DavidRMorphew <morphewd@umich.edu>
2021-06-28 08:17:14 -0400

    extraneous routes created by controller generated removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'parts/index'
-  get 'set_part_specs/index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lego_sets, only: [:index, :update] do
     resources :set_part_specs, only: [:index]
