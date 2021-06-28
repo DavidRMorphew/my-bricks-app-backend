327cb2d28f4f0ee40d3238767681861a36e0f748
DavidRMorphew <morphewd@umich.edu>
2021-06-28 18:16:39 -0400

    custom route for 'lego_sets/owned' moved to above resources 'lego_sets' to avoid potential conflict with show route of lego_sets in future use

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
+  get "/lego_sets/owned", to: "lego_sets#owned_sets", as: "owned_sets"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lego_sets, only: [:index, :update] do
     resources :set_part_specs, only: [:index]
     resources :parts, only: [:index]
   end
 
-  get "/lego_sets/owned", to: "lego_sets#owned_sets", as: "owned_sets"
 end
