c6f4740133e6ccff3c6a1dda91a97378a44b8fe1
DavidRMorphew <morphewd@umich.edu>
2021-06-29 14:09:41 -0400

    strictParam dynamic segment added to potential_builds route in config routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   get "/lego_sets/owned", to: "lego_sets#owned_sets", as: "owned_sets"
-  get "/lego_sets/potential_builds", to: "lego_sets#potential_builds", as: "potential_builds"
+  get "/lego_sets/potential_builds/:strictParam", to: "lego_sets#potential_builds", as: "potential_builds"
 
   resources :lego_sets, only: [:index, :update] do
     resources :set_part_specs, only: [:index]
