3e358afa4e43d5066b932cfdc7c5d6de9d4a41d5
DavidRMorphew <morphewd@umich.edu>
2021-06-29 22:07:06 -0400

    README corrected and seeds file rearranged

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 castle_themes_nums = [25, 186, 187, 188, 189, 190, 191, 193, 194, 195, 196, 197, 198, 199, 201, 202, 203, 211, 255]
-pirate_themes_nums = [147, 148, 149, 151, 152, 153, 154, 215]
 space_theme_nums = [93, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 144, 146, 452, 527]
 classic_town_nums = [68, 69, 70, 71, 72, 73, 74, 76, 77, 78, 79, 80, 82, 83, 85]
+pirate_themes_nums = [147, 148, 149, 151, 152, 153, 154, 215]
 
 # Select which Lego themes you would like to use to add to the database.
 
@@ -9,19 +9,19 @@ classic_town_nums = [68, 69, 70, 71, 72, 73, 74, 76, 77, 78, 79, 80, 82, 83, 85]
     # castle_themes_nums.each do |theme_num|
     #     Api.fetch_sets_by_theme_num(theme_num)
     # end
-
-# For Pirate themes, uncomment the following:
-    # pirate_themes_nums.each do |theme_num|
-    #     Api.fetch_sets_by_theme_num(theme_num)
-    # end
-
+    
 # For Space themes, uncomment the following:
     # space_theme_nums.each do |theme_num|
     #     Api.fetch_sets_by_theme_num(theme_num)
     # end
-
+    
 # For Classic Town themes, uncomment the following:
     # classic_town_nums.each do |theme_num|
+    #     Api.fetch_sets_by_theme_num(theme_num)
+    # end
+    
+# For Pirate themes, uncomment the following:
+    # pirate_themes_nums.each do |theme_num|
     #     Api.fetch_sets_by_theme_num(theme_num)
     # end
 
