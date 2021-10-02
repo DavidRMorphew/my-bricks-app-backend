bce226522ceed1e0a834a076929e95ffc2d69575
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-02 09:46:02 -0400

    add has_secure_password & bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     validates :email, uniqueness: {case_sensitive: false}
     before_validation :downcase_email
+    has_secure_password
 
     def downcase_email
         self.email = self.email.downcase
