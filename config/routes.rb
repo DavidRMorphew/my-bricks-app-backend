c3a4a94529db8316ba775399f23344d86321b148
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-02 09:36:26 -0400

    add user model, controller, table, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :users
   get "/lego_sets/owned", to: "lego_sets#owned_sets", as: "owned_sets"
   get "/lego_sets/potential_builds/:strictParam", to: "lego_sets#potential_builds", as: "potential_builds"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_204400) do
+ActiveRecord::Schema.define(version: 2021_10_02_133536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 2021_06_27_204400) do
   create_table "themes", force: :cascade do |t|
     t.string "theme_number"
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
