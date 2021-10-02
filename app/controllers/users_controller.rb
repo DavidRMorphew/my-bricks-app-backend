abf916fda921dead8908844e0c4a0d4a822587f0
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-02 19:18:00 -0400

    add logic for user validation at creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
     wrap_parameters :user, include: [:name, :email, :password]
     
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            # register user with token
+        end
     end
 
     private
