b810af831c75293df9ca7cc92720d1d5f729ad4f
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-02 09:43:52 -0400

    add before_validation to downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     validates :email, uniqueness: {case_sensitive: false}
+    before_validation :downcase_email
+
+    def downcase_email
+        self.email = self.email.downcase
+    end
 end
