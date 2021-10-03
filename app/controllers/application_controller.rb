8556bc62857b65ecf3dfbcdd9a497e0ff35f28d8
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-03 17:13:50 -0400

    add payload parameter missing in encode_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    def encode_token
+    def encode_token(payload)
         JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
