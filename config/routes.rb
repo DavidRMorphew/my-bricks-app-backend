d8e59872b5fe1369f6c777ca7af2bd181f4e09cf
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-03 16:12:36 -0400

    add route for login set to auth#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   resources :users
+
+  get '/login', to: 'auth#create'
+  # update these
   get "/lego_sets/owned", to: "lego_sets#owned_sets", as: "owned_sets"
   get "/lego_sets/potential_builds/:strictParam", to: "lego_sets#potential_builds", as: "potential_builds"
 
