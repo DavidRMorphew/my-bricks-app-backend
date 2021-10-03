674b31458430b570b3be1406fb6d2eaab495f258
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-03 17:12:57 -0400

    add decoded_token to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::API
     def encode_token
         JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
+
+    def decoded_token
+        if request.headers['Authorization']
+          token = request.headers['Authorization'].split(' ')[1]
+          begin
+            JWT.decode(token, Rails.application.secrets.secret_key_base, 
+                true, algorithm: 'HS256')
+          rescue JWT::DecodeError
+            nil
+          end
+        end
+    end
 end
