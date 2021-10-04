899383f78298a5e3078ea73a53e0b4ab4f1cbf33
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-04 18:09:28 -0400

    add login_params and wrap_params to auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,11 +1,19 @@
 class AuthController < ApplicationController
+  wrap_parameters :user, include: [:email, :password]
+  
   def create
-    
+
   end
 
   def destroy
   end
 
   def logged_in
+  end
+
+  private
+
+  def login_params
+      params.require(:user).permis(:email, :password)
   end
 end
