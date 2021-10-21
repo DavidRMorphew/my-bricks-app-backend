a5e33791dbe10ce8c5cc7bab798f35958286edff
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-20 20:55:54 -0400

    add validation test for password and email of user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe User, type: :model do
   it "requires the presence of a name" do
     expect(User.new(email: "billy@billybob.com", password: "pw")).not_to be_valid
   end
+
+  it "requires the presence of a password" do
+    expect(User.new(name: "Bill", email: "billy@billybob.com")).not_to be_valid
+  end
+
+  it "requires the presence of an email address" do
+    expect(User.new(name: "Billy", password: "pw")).not_to be_valid
+  end
 end
