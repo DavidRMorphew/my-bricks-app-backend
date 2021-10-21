37c64fd2fbe75a6ed107fee84b75d3ee6a2809b9
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-21 13:54:41 -0400

    tests added for email uniqueness and case-insensitive uniqueness of user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
+  let(:user1_downcase) {
+    User.create(
+      name: "Bob",
+      email: "bobman@bobby.com",
+      password: "pw"
+    )
+  }
+  
+  let(:user1_downcase_duplicate) {
+    User.new(
+      name: "Bob",
+      email: "bobman@bobby.com",
+      password: "pw"
+    )
+  }
+
+  let(:user1_uppercase) {
+    User.new(
+      name: "Bob",
+      email: "BOBMAN@bobby.com",
+      password: "pw"
+    )
+  }
+
+
   it "requires the presence of a name" do
     expect(User.new(email: "billy@billybob.com", password: "pw")).not_to be_valid
   end
@@ -11,5 +37,15 @@ RSpec.describe User, type: :model do
 
   it "requires the presence of an email address" do
     expect(User.new(name: "Billy", password: "pw")).not_to be_valid
+  end
+
+  it "requires that each email be unique" do
+    expect(user1_downcase).to be_valid
+    expect(user1_downcase_duplicate).not_to be_valid
+  end
+
+  it "requires that each email be unique and case-insensitive" do
+    expect(user1_downcase).to be_valid
+    expect(user1_uppercase).not_to be_valid
   end
 end
