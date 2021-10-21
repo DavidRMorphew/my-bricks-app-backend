aa58574557c7df5f5334d7cc108a6f3ba45d7999
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-21 13:36:30 -0400

    add conditional to downcase_email before_validation to check email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, uniqueness: {case_sensitive: false}
-    validates :name, presence: true
+    validates :name, :email, presence: true
     before_validation :downcase_email
+    validates :email, uniqueness: {case_sensitive: false}
     has_many :owned_sets
     has_many :lego_sets, through: :owned_sets
 
     def downcase_email
-        self.email = self.email.downcase
+        self.email = self.email.downcase if self.email
     end
 end
