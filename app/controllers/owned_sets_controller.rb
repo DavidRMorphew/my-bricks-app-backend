b043cdc7d7b95730c330bf47c000f8776773ad94
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-21 22:20:47 -0400

    add create action to OwnedSetsController

--- app/controllers/owned_sets_controller.rb
+++ app/controllers/owned_sets_controller.rb
@@ -5,6 +5,12 @@ class OwnedSetsController < ApplicationController
   end
 
   def create
+    owned_set = current_user.owned_sets.build(lego_set_id: params[:legoSetId])
+    if owned_set.save
+      render json: OwnedSetsSerializer.new(owned_set)
+    else
+      render json: { error: 'failed to add set to owned'}, status: :not_acceptable
+    end
   end
 
   def destroy
